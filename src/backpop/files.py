96452c6ed80b90d2485d7c7b2f0f0a6a44bf197c
Hannah Skobe <hskobe@sirius.ib.vera.psc.edu>
2026-03-18 16:22:15 -0400

    Fixed use_bcm = True condition for bcm_columns

--- src/backpop/files.py
+++ src/backpop/files.py
@@ -119,7 +119,7 @@ def parse_inifile(ini_file):
     else:
         config["bpp_columns"] = BPP_COLUMNS
         
-    if use_bcm:
+    if config["use_bcm"] == "true":
         if config["bcm_columns"] != "" and config["bcm_columns"] != "None":
             # make sure bcm_columns names are found in BCM_COLUMNS
             config["bcm_columns"] = eval(config["bcm_columns"])
